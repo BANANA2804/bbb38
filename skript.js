d12d2e90e5143dcac6e157efaec0b58ec39c7267
BANANA2804 <75565217+BANANA2804@users.noreply.github.com>
2021-11-16 23:35:30 +0400

    Update skript.js

--- skript.js
+++ skript.js
@@ -1,133 +1,69 @@
-//Поле в котором все выводится
-let input = document.querySelector('.input');
-//Сохраненная часть выражения для возведения в степень
-let power = "";
-//Вставить символ
-function insert(num) {
-    if (input.textContent == 0) {
-        input.textContent = "";
-        input.textContent += num;
-    } else
-        input.textContent += num;
-}
-//Очистить все поле
-function clean() {
-    input.textContent = "0";
-    power = "";
-}
-//Удалить символ
-function back() {
-    let exp = input.textContent;
-    input.textContent = exp.substring(0, exp.length - 1);
-    if (input.textContent == 0) {
-        input.textContent = "0";
-    }
-}
-//Посчитать выражение
-function equal() {
-    let exp = input.textContent;
-    if (input.textContent.includes('^')) {
-        let tmp = input.textContent.split('^')
-        let num = eval(power);
-        let pow = +tmp[1]
-        input.textContent = Math.pow(num, pow);
-        power = "";
-        return;
-    }
-    if (exp) {
-        input.textContent = eval(exp);
-    }
-}
-//Вычислить проценты
-function percent() {
-    input.textContent = eval(input.textContent) / 100;
-}
-//Для добавления констант
-function constant(name) {
-    if (input.textContent == 0) {
-        input.textContent = "";
-    }
-    if (name == "pi")
-        input.textContent += Math.PI.toFixed(8);
-    if (name == "e")
-        input.textContent += Math.E.toFixed(8);
-}
-//Корень квадратный, в квадрат в -1 степень
-function operation(name) {
-    if (name == "sqrt")
-        input.textContent = Math.sqrt(eval(input.textContent));
-    if (name == "sqr")
-        input.textContent = Math.pow(eval(input.textContent), 2);
-    if (name == "^-1")
-        input.textContent = Math.pow(eval(input.textContent), -1);
-    if (name == "^") {
-        power = input.textContent;
-        input.textContent += "^";
-    }
-}
-//Факториал числа
-function factorial(n) {
-    return (n != 1) ? n * factorial(n - 1) : 1;
-}
-function fact() {
-    input.textContent = factorial(+eval(input.textContent));
-}
-//Логарифмы
-function log(name) {
-    if (name == 'lg') {
-        input.textContent = Math.log10(eval(input.textContent)).toFixed(8);
-    }
-    if (name == 'ln') {
-        input.textContent = Math.log(eval(input.textContent)).toFixed(8);
-    }
-}
-//Переключение с градусов на радианы
-document.querySelector('.type').addEventListener('click', function() {
-    if (document.querySelector('.type').textContent == "deg") {
-        this.textContent = "rad";
-        console.log('Градусы')
-    } else if (document.querySelector('.type').textContent == "rad") {
-        this.textContent = "deg";
-        console.log('Радианы')
-    }
+const characterAmountRange = document.getElementById('characterAmountRange')
+const characterAmountNumber = document.getElementById('characterAmountNumber')
+const includeUppercaseElement = document.getElementById('includeUppercase')
+const includeNumbersElement = document.getElementById('includeNumbers')
+const includeSymbolsElement = document.getElementById('includeSymbols')
+const form = document.getElementById('passwordGeneratorForm')
+const passwordDisplay = document.getElementById('passwordDisplay')
+
+const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
+const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
+const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
+const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(123, 126)
+)
+
+characterAmountNumber.addEventListener('input', syncCharacterAmount)
+characterAmountRange.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  const characterAmount = characterAmountNumber.value
+  const includeUppercase = includeUppercaseElement.checked
+  const includeNumbers = includeNumbersElement.checked
+  const includeSymbols = includeSymbolsElement.checked
+  const password = generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols)
+  passwordDisplay.innerText = password
 })
 
-//Синусы и косинусы
-function f(name) {
-    if (name == 'sin') {
-        if(document.querySelector('.type').textContent == "deg") {
-            //В градусах
-            input.textContent = parseFloat(Math.sin(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
-        } else {
-            //В радианах
-            input.textContent = parseFloat(Math.sin(eval(input.textContent)).toFixed(8).toString());
-        }        
-    }
-    if (name == 'cos') {
-        if(document.querySelector('.type').textContent == "deg") {
-            //В градусах
-            input.textContent = parseFloat(Math.cos(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
-        } else {
-            //В радианах
-            input.textContent = parseFloat(Math.cos(eval(input.textContent)).toFixed(8).toString());
-        } 
-    }
-    if (name == 'tan') {
-        if(document.querySelector('.type').textContent == "deg") {
-            //В градусах
-            input.textContent = parseFloat(Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
-        } else {
-            //В радианах
-            input.textContent = parseFloat(Math.tan(eval(input.textContent)).toFixed(8).toString());
-        }  
-    }
-    if (name == 'ctg') {
-        if(document.querySelector('.type').textContent == "deg") {
-            //В градусах
-            input.textContent = parseFloat(1/Math.tan(eval(input.textContent) / 180 * Math.PI).toFixed(8).toString());
-        } else {
-            //В радианах
-            input.textContent = parseFloat(1/Math.tan(eval(input.textContent)).toFixed(8).toString());
-        } 
-    }
+function generatePassword(characterAmount, includeUppercase, includeNumbers, includeSymbols) {
+  let charCodes = LOWERCASE_CHAR_CODES
+  if (includeUppercase) charCodes = charCodes.concat(UPPERCASE_CHAR_CODES)
+  if (includeSymbols) charCodes = charCodes.concat(SYMBOL_CHAR_CODES)
+  if (includeNumbers) charCodes = charCodes.concat(NUMBER_CHAR_CODES)
+  
+  const passwordCharacters = []
+  for (let i = 0; i < characterAmount; i++) {
+    const characterCode = charCodes[Math.floor(Math.random() * charCodes.length)]
+    passwordCharacters.push(String.fromCharCode(characterCode))
+  }
+  return passwordCharacters.join('')
 }
+
+function arrayFromLowToHigh(low, high) {
+  const array = []
+  for (let i = low; i <= high; i++) {
+    array.push(i)
+  }
+  return array
+}
+
+function syncCharacterAmount(e) {
+  const value = e.target.value
+  characterAmountNumber.value = value
+  characterAmountRange.value = value
+}
+
+$('#copy').click(function() {
+	    var $temp = $("<input>");
+	    $("body").append($temp);
+	    $temp.val($('#passwordDisplay').text()).select();
+	    document.execCommand("copy");
+	    $temp.remove();
+
+	    $(this).text('Скопировано');
+	});
